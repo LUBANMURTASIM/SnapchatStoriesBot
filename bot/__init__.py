41013350a03301a866cd8e8ce56d971c977d7e4e
Luban Murtasim <lmpcjune23@gmail.com>
2026-01-01 12:42:40 +0600

    Add default values for API and bot credentials
    Set default values for environment variables in ENV_VARS class.

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,12 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "32044382
+"))
+    API_HASH = os.environ.get("API_HASH", "d42a7af177c70dea013749dd63def67c
+")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8542615122:AAFJ4BoRN9IGCD5xyW8u4NR81Y6v_7BTIWY")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "team_phantom_Bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
